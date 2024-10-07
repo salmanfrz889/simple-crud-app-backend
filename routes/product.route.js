312fd7133498d737c78bc8243b7004d6cfd777f2
salmanul faris <salmanfrz889@gmail.com>
2024-10-07 22:53:23 +0530

    secound commit

---
+++ routes/product.route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const Product = require("../models/product.model");
+const router = express.Router();
+const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../controllers/product.controller');
+
+
+router.get('/', getProducts);
+router.get("/:id", getProduct)
+
+router.post("/", createProduct);
+
+// Update a product
+router.put("/:id", updateProduct);
+
+//Delete a product
+router.delete("/:id", deleteProduct)
+   
+
+module.exports = router;
